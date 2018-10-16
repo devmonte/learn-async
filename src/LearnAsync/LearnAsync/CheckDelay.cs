f94a37894dc1d721472c9d1f21111bea521e2a6e
devmonte <greg.jonca@gmail.com>
2018-10-16 15:23:56 +0200

    Add base check of async method

---
+++ src/LearnAsync/LearnAsync/CheckDelay.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearnAsync
+{
+    public class SimpleCheckAsync
+    {
+        public void Check()
+        {
+            Console.WriteLine($"\n {DateTime.Now} Start check!");
+            LongRunningMethod();
+            Console.WriteLine($"\n {DateTime.Now} Wait!");
+            Task.Delay(TimeSpan.FromSeconds(3)).Wait();
+            Console.WriteLine($"\n {DateTime.Now} Stop check!");
+        }
+
+        public async Task CheckAsync()
+        {
+            Console.WriteLine($"\n {DateTime.Now} {Thread.CurrentThread.ManagedThreadId}  Start check async!");
+            var taskLongRuning = LongRunningMethodAsync();
+            Console.WriteLine($"\n {DateTime.Now} {Thread.CurrentThread.ManagedThreadId}  Wait!");
+            await taskLongRuning;
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            Console.WriteLine($"\n {DateTime.Now} {Thread.CurrentThread.ManagedThreadId}  Stop check async!");
+        }
+
+        public async Task CheckTaskFactory()
+        {
+            Console.WriteLine($"\n {DateTime.Now} {Thread.CurrentThread.ManagedThreadId}  Start check async!");
+            var taskLongRunning = Task.Factory.StartNew(LongRunningMethodAsync);
+            Console.WriteLine($" {DateTime.Now} {Thread.CurrentThread.ManagedThreadId}  Wait!");
+            await taskLongRunning;
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            Console.WriteLine($"\n {DateTime.Now} {Thread.CurrentThread.ManagedThreadId}  Stop check async!");
+        }
+
+        private void LongRunningMethod()
+        {
+            Console.Write($"\n {DateTime.Now} {Thread.CurrentThread.ManagedThreadId} Long running method start");
+            Task.Delay(TimeSpan.FromSeconds(3)).Wait();
+            Console.Write($"\n {DateTime.Now} {Thread.CurrentThread.ManagedThreadId} Long running method stop");
+        }
+
+        private async Task LongRunningMethodAsync()
+        {
+            Console.Write($"\n {DateTime.Now} {Thread.CurrentThread.ManagedThreadId} Long running method start");
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            Console.Write($"\n {DateTime.Now} {Thread.CurrentThread.ManagedThreadId} Long running method stop");
+        }
+
+    }
+}
---
+++ src/LearnAsync/LearnAsync/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LearnAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var checkDelay = new SimpleCheckAsync();
+
+            checkDelay.Check();
+            checkDelay.CheckAsync().Wait();
+            checkDelay.CheckTaskFactory().Wait();
+
+            Console.ReadLine();
+        }
+    }
+}
